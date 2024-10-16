36869c3e38f170b007c1414a38e7e7dc07eef87a
RyanS <ryanweeney157@gmail.com>
2024-10-16 18:35:37 -0400

    adding more functionalities

--- java.js
+++ java.js
@@ -21,6 +21,12 @@ for (var i = 0; i < quantityInputs.length; i++) {
     var input = quantityInputs[i]
     input.addEventListener('change', quantityChanged)
 }
+
+var addToCartButtons = document.getElementsByClassName('shop-item-button')
+for (var i = 0; i < addToCartButtons.length; i++) {
+    var button = addToCartButtons[i]
+    button.addEventListener('click', addToCartClicked)
+}
 }
 
 function removeCartItem(event) {
@@ -28,6 +34,32 @@ function removeCartItem(event) {
     // getting rid of entire section so it calls the parent element of the parent element
     buttonClicked.parentElement.parentElement.remove()
     updateCartTotal()
+}
+
+
+//makes sure it doesn't go below 1
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+     var button = event.target
+     var shopItem = button.parentElement.parentElement
+     var title = shopItem.getElementsByClassName('shop-item-title')[0].innertext
+     var price = shopItem.getElementsByClassName('shop-item-price')[0].innertext
+     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+     console.log(title, price, imageSrc)
+    
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    cartItems.append(cartRow)
 }
 
 function updateCartTotal() {
@@ -41,6 +73,8 @@ function updateCartTotal() {
         var price = parseFloat(priceElement.innerText.replace('$', ''))
         var quantity = quantityElement.value
         total = total + (price * quantity)
+
     }
+    total = Math.round(total * 100) / 100
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 }
