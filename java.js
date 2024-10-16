fba3748f2dbd939ae80f4364f0f3fe8841dd55fc
RyanS <ryanweeney157@gmail.com>
2024-10-16 11:09:45 -0400

    added more java

--- java.js
+++ java.js
@@ -1,11 +1,26 @@
-var removeItemButtons = document.getElementsByClassName("btn-danger")
+var removeItemButtons = document.getElementsByClassName('btn-danger')
 console.log(removeItemButtons)
 // loop through all different buttons in cart
 for (var i = 0; i < removeItemButtons.length; i++) {
     var button = removeItemButtons[i]
-    button.addEventListener("click", function(event) {
-        var buttonClicked =  event.target
+    button.addEventListener('click', function(event) {
+        var buttonClicked =  event.target 
         // getting rid of entire section so it calls the parent element of the parent element
         buttonClicked.parentElement.parentElement.remove()
     })
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName('cart-total-price')[0].innerText = total
 }
